b7e64f436419a09f395f8ebef8d8bba953435986
David <dcruzhernadez5046@panther.chaffey.edu>
2020-03-05 20:04:17 -0800

    For Game Project 1 Dashers

---
+++ Dashers/Assets/Assets/BasicAI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicAI : MonoBehaviour
+{
+    public float speed = 5;
+    public Transform target;
+    public Transform myTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.LookAt(target);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(target);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+}
---
+++ Dashers/Assets/Assets/playerdamage.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playerdamage : MonoBehaviour
+{
+    public Slider playerHealth ;
+    public GameObject Dead_panel ;
+    float health = 10;
+
+    void Start()
+    {
+        Dead_panel.gameObject.SetActive(false);
+    }
+    void Update()
+    {
+        playerHealth.value = health;
+        if (health <= 0)
+        {
+         health = 0;
+            Dead_panel.gameObject.SetActive(true);
+         Destroy(gameObject);
+        }
+        }
+     private void OnCollisionEnter(Collision co)
+    {
+        if (co.gameObject.tag == "COP")
+        {
+            health -= 5;
+        }
+    }
+}
